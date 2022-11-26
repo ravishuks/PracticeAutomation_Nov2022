327f659193cb3b9bd56404ad3c4f98d8de19c979
ravshukla <ravshukla@USBLRRAVSHUKLA9.us.deloitte.com>
2022-11-26 20:49:30 +0530

    Checkin the first code

---
+++ PracticeProject_Nov2022/src/day1/GreatestOfThreeNumbers.java
@@ -0,0 +1,35 @@
+package day1;
+
+public class GreatestOfThreeNumbers {
+
+	public static void main(String[] args) {
+
+		int firstNum;
+		int secondNum;
+		int thirdNum;
+		
+		firstNum = 30;
+		secondNum= 3;
+		thirdNum= 30;
+		
+		if (firstNum >= secondNum && firstNum >= thirdNum  ) 
+		{
+			System.out.println("The largest number is : " + firstNum);
+		} 
+		
+		else if (secondNum >= firstNum && secondNum >= thirdNum)
+		{
+		
+			System.out.println("The largest number is : " + secondNum);
+		}
+		else if (thirdNum>= firstNum || thirdNum >= secondNum) 
+		{
+				System.out.println("The largest number is : " + thirdNum);
+				
+		}
+		
+		
+
+	}
+
+}
